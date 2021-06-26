deb61076277f2a48856720641a2c4bd462f2d7a1
Arief Syahroni <riefsyah31@gmail.com>
2021-06-26 20:15:17 +0700

    Add home page and Modified main page

--- student_room/lib/main.dart
+++ student_room/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_room/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
