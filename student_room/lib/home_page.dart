6551da9a644e303b38898965dc29a2f9d368e3f0
Arief Syahroni <riefsyah31@gmail.com>
2021-06-26 21:20:59 +0700

    Add content home page

--- student_room/lib/home_page.dart
+++ student_room/lib/home_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text('Body Page'),
+      ),
+    );
   }
 }
