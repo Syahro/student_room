880e034d56a3b4118332b84b3a0505619d66b0dc
Arief Syahroni <riefsyah31@gmail.com>
2021-06-26 20:56:57 +0700

    Modified Splash Screen and main page

--- student_room/lib/main.dart
+++ student_room/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:student_room/splash_page.dart';
+import 'package:student_room/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        backgroundColor: backgroundColor,
+      ),
       home: SplashPage(),
     );
   }
--- student_room/lib/splash_page.dart
+++ student_room/lib/splash_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:student_room/home_page.dart';
+import 'package:student_room/theme.dart';
 
 class SplashPage extends StatelessWidget {
   @override
@@ -8,9 +10,68 @@ class SplashPage extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(
-              height: 80,
+              height: 60,
             ),
-            Image.asset('assets/images/logo.png'),
+            Image.asset(
+              'assets/images/logo.png',
+              height: 80,
+              width: 80,
+            ),
+            SizedBox(
+              height: 56,
+            ),
+            Image.asset(
+              'assets/images/ilustration.png',
+              width: 267,
+              height: 200,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            Text(
+              'Up Your Skills',
+              style: titleTextStyle.copyWith(
+                fontSize: 28,
+                color: blackColor,
+              ),
+            ),
+            SizedBox(
+              height: 7,
+            ),
+            Text(
+              'We provide content that helps\neveryone to learn anything',
+              style: subTitleTextStyle.copyWith(
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+                color: greyColor,
+              ),
+            ),
+            SizedBox(
+              height: 56,
+            ),
+            Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width - (2 * 24),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return HomePage();
+                  }));
+                },
+                color: blueColorButton,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(17),
+                ),
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                    color: whiteColorButton,
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
