ee0da8ce700e1c4d5a181e00bc80d402a3f15968
Arief Syahroni <riefsyah31@gmail.com>
2021-06-26 20:20:59 +0700

    Add Splash page and Modified main page

--- student_room/lib/main.dart
+++ student_room/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:student_room/home_page.dart';
+import 'package:student_room/splash_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
