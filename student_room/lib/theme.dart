f7ce0054625654af24905b5d380c8139d0366b5a
Arief Syahroni <riefsyah31@gmail.com>
2021-06-26 20:08:23 +0700

    Update Assets and Theme

---
+++ student_room/lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = Color(0XFFF7F8FB);
+Color blackColor = Color(0XFF161822);
+Color greyColor = Color(0XFF737583);
+Color blueColorButton = Color(0XFF3B3DBF);
+Color whiteColorButton = Color(0XFFF7F8FB);
+Color greyColorButton = Color(0XFFECEEF5);
+Color darkGreyColorButton = Color(0XFFC3C8DA);
