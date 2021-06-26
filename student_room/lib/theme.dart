23bff81d734cb265a8fe45e477e0a50ddc4e7556
Arief Syahroni <riefsyah31@gmail.com>
2021-06-26 20:13:37 +0700

    Update Teheme and pubspec

--- student_room/lib/theme.dart
+++ student_room/lib/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color backgroundColor = Color(0XFFF7F8FB);
 Color blackColor = Color(0XFF161822);
@@ -7,3 +8,13 @@ Color blueColorButton = Color(0XFF3B3DBF);
 Color whiteColorButton = Color(0XFFF7F8FB);
 Color greyColorButton = Color(0XFFECEEF5);
 Color darkGreyColorButton = Color(0XFFC3C8DA);
+
+TextStyle titleTextStyle = GoogleFonts.tajawal(
+  fontWeight: FontWeight.w500,
+  fontSize: 12,
+);
+
+TextStyle subTitleTextStyle = GoogleFonts.tajawal(
+  fontWeight: FontWeight.w300,
+  fontSize: 16,
+);
